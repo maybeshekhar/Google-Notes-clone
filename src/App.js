0f419002a957a5ecbf1a79fdda7a16ee18dd7977
Chandrashekhar Jenamoni <shekharmaybe@gmail.com>
2023-01-26 01:38:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, createContext } from "react";
+
+import NoteContainer from "./Components/NoteContainer/NoteContainer";
+import Sidebar from "./Components/Sidebar/Sidebar";
+
+import ReactSwitch from "react-switch";
+
+
+import "./App.css";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+   };
+
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("notes-app")) || []
+  );
+
+  const addNote = (color) => {
+    const tempNotes = [...notes];
+
+    tempNotes.push({
+      id: Date.now() + "" + Math.floor(Math.random() * 78),
+      text: "",
+      time: Date.now(),
+      color,
+    });
+    setNotes(tempNotes);
+  };
+
+  const deleteNote = (id) => {
+    const tempNotes = [...notes];
+
+    const index = tempNotes.findIndex((item) => item.id === id);
+    if (index < 0) return;
+
+    tempNotes.splice(index, 1);
+    setNotes(tempNotes);
+  };
+
+  const updateText = (text, id) => {
+    const tempNotes = [...notes];
+
+    const index = tempNotes.findIndex((item) => item.id === id);
+    if (index < 0) return;
+
+    tempNotes[index].text = text;
+    setNotes(tempNotes);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("notes-app", JSON.stringify(notes));
+  }, [notes]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeContext.Provider value={{ theme, toggleTheme}}>
+    <div className="App" id={theme}>
+      
+      <Sidebar addNote={addNote} />
+     
+      <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+      
+      <NoteContainer
+        notes={notes}
+        deleteNote={deleteNote}
+        updateText={updateText}
+      />
     </div>
+    </ThemeContext.Provider>
   );
 }
 
